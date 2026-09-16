9edc6458a85612a90ad72d52a2bf0c68a0664fc1
jesusmunzon <jesusmunzon@gmail.com>
2026-09-16 11:56:48 +0200

    archivos organizados

---
+++ config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                    },
+                    colors: {
+                        brand: {
+                            50: '#f0f7ff',
+                            100: '#e0effe',
+                            500: '#0284c7',
+                            600: '#0369a1',
+                            700: '#075985',
+                            900: '#0c4a6e',
+                        }
+                    }
+                }
+            }
+        };
