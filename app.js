ccc33b1c27ac77ef53592456024b71d6b94c6e79
jesusmunzon <jesusmunzon@gmail.com>
2026-09-19 16:15:48 +0200

    Add function to export summary as PDF

--- app.js
+++ app.js
@@ -759,6 +759,10 @@
             updateEntronqueUI();
         }
 
+        function exportSummaryPDF() {
+            window.print();
+        }
+
         function normalizeSheetName(name) {
             return String(name || '').normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
         }
